19162a2fd7f519878d264e29dde68ad4afc874a8
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-19 02:25:59 -0500

    Added asset manager, replaced tabs with spaces

--- core/src/com/taugames/freefall/Game.java
+++ core/src/com/taugames/freefall/Game.java
@@ -1,28 +1,37 @@
 package com.taugames.freefall;
 
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends com.badlogic.gdx.Game {
-	private SpriteBatch spriteBatch;
-	
-	@Override
-	public void create() {
-		spriteBatch = new SpriteBatch();
-		this.setScreen(new MainMenu(this));
-	}
+    private SpriteBatch spriteBatch;
+    private AssetManager assetManager;
 
-	@Override
-	public void render() {
-	    super.render();
-	}
-	
-	@Override
-	public void dispose() {
-		spriteBatch.dispose();
-		this.getScreen().dispose();
-	}
+    @Override
+    public void create() {
+        spriteBatch = new SpriteBatch();
+        assetManager = new AssetManager();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+        if (assetManager.update()) {
+            this.setScreen(new MainMenu(this));
+        }
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+        this.getScreen().dispose();
+    }
 
     public SpriteBatch getSpriteBatch() {
         return spriteBatch;
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager;
     }
 }
