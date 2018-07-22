dce1d5f799ddfd82a7464c7a6d15d74f60e9001c
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-22 02:17:32 -0500

    Added split laser functionality

--- core/src/com/taugames/freefall/obstacles/RandomObstacleGenerator.java
+++ core/src/com/taugames/freefall/obstacles/RandomObstacleGenerator.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.taugames.freefall.Game;
 import com.taugames.freefall.obstacles.lasers.DoubleLaser;
 import com.taugames.freefall.obstacles.lasers.SingleLaser;
+import com.taugames.freefall.obstacles.lasers.SplitLaser;
 
 public class RandomObstacleGenerator {
     private final Game game;
@@ -20,7 +21,7 @@ public class RandomObstacleGenerator {
 
     public SingleLaser getSingleLaser(float y) {
         SingleLaser.Direction direction = Math.random() >= 0.5 ? SingleLaser.Direction.LEFT : SingleLaser.Direction.RIGHT;
-        float gap = (float) Math.random() * (Gdx.graphics.getWidth() / 16f) + (3f / 16) * Gdx.graphics.getWidth();
+        float gap = (float) Math.random() * Gdx.graphics.getWidth() / 16f + 3f / 16 * Gdx.graphics.getWidth();
         float x = direction == SingleLaser.Direction.LEFT ? Gdx.graphics.getWidth() - gap : gap;
         float width = direction == SingleLaser.Direction.LEFT ? x : Gdx.graphics.getWidth() - gap;
         float height = Gdx.graphics.getWidth() / 32f;
@@ -30,7 +31,15 @@ public class RandomObstacleGenerator {
     public DoubleLaser getDoubleLaser(float y) {
         float width = (float) Math.random() * Gdx.graphics.getWidth() / 8f + Gdx.graphics.getWidth() / 4f;
         float height = Gdx.graphics.getWidth() / 32f;
-        float x = (float) Math.random() * ((3f/ 4) * Gdx.graphics.getWidth() - width) + Gdx.graphics.getWidth() / 8f + width / 2;
+        float x = (float) Math.random() * (3f / 4 * Gdx.graphics.getWidth() - width) + Gdx.graphics.getWidth() / 8f + width / 2;
         return new DoubleLaser(game, x, y, width, height, velocity);
+    }
+
+    public SplitLaser getSplitLaser(float y) {
+        float leftGap = (float) Math.random() * 3f / 80 * Gdx.graphics.getWidth() + (11f / 80) * Gdx.graphics.getWidth();
+        float rightGap = (float) Math.random() * 3f / 80 * Gdx.graphics.getWidth() + (11f / 80) * Gdx.graphics.getWidth();
+        float width = Gdx.graphics.getWidth() - leftGap - rightGap;
+        float height = Gdx.graphics.getWidth() / 32f;
+        return new SplitLaser(game, leftGap, y, width, height, velocity);
     }
 }
--- core/src/com/taugames/freefall/obstacles/lasers/SplitLaser.java
+++ core/src/com/taugames/freefall/obstacles/lasers/SplitLaser.java
@@ -1,22 +1,21 @@
 package com.taugames.freefall.obstacles.lasers;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.taugames.freefall.Game;
 import com.taugames.freefall.Parachutist;
 import com.taugames.freefall.obstacles.Obstacle;
 
-public class DoubleLaser extends Obstacle {
+public class SplitLaser extends Obstacle {
     private Texture laserBody;
-    private Texture laserEndLeft;
     private Texture laserEndRight;
+    private Texture laserEndLeft;
 
-    public DoubleLaser(Game game, float x, float y, float width, float height, float velocity) {
+    public SplitLaser(Game game, float x, float y, float width, float height, float velocity) {
         super(game, x, y, width, height, velocity);
         laserBody = game.getAssetManager().get("img/lasers/laserBody.png", Texture.class);
-        laserEndLeft = game.getAssetManager().get("img/lasers/laserEndLeft.png", Texture.class);
         laserEndRight = game.getAssetManager().get("img/lasers/laserEndRight.png", Texture.class);
+        laserEndLeft = game.getAssetManager().get("img/lasers/laserEndLeft.png", Texture.class);
     }
 
     @Override
@@ -34,9 +33,8 @@ public class DoubleLaser extends Obstacle {
         if (!spriteBatch.isDrawing()) {
             spriteBatch.begin();
         }
-        spriteBatch.draw(laserEndLeft, x - width / 2 - height, y, height, height);
-        spriteBatch.draw(laserEndRight, x + width / 2, y, height, height);
-        spriteBatch.draw(laserBody, 0, y, x - width / 2 - height, height);
-        spriteBatch.draw(laserBody, x + width / 2 + height, y, Gdx.graphics.getWidth() - (x + width / 2 + height), height);
+        spriteBatch.draw(laserEndRight, x, y, height, height);
+        spriteBatch.draw(laserBody, x + height, y, width - 2 * height, height);
+        spriteBatch.draw(laserEndLeft, x + width - height, y, height, height);
     }
 }
