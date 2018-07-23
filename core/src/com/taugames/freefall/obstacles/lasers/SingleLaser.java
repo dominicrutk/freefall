ada16aff30878fc6490d930071d5c585e5b1406a
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-23 00:22:26 -0500

    Added collision detection and handling for SingleLasers

--- core/src/com/taugames/freefall/obstacles/lasers/SingleLaser.java
+++ core/src/com/taugames/freefall/obstacles/lasers/SingleLaser.java
@@ -3,6 +3,8 @@ package com.taugames.freefall.obstacles.lasers;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Polygon;
 import com.taugames.freefall.Game;
 import com.taugames.freefall.Parachutist;
 import com.taugames.freefall.obstacles.Obstacle;
@@ -25,7 +27,48 @@ public class SingleLaser extends Obstacle {
 
     @Override
     public boolean kills(Parachutist parachutist) {
-        return false;
+        Polygon model;
+        float px = height / 8;
+        if (direction == Direction.LEFT) {
+            model = new Polygon(new float[] {
+                    0, y + 6 * px,
+                    width - 8 * px, y + 6 * px,
+                    width - 8 * px, y + 7 * px,
+                    width - 6 * px, y + 7 * px,
+                    width - 6 * px, y + 8 * px,
+                    width - 1 * px, y + 8 * px,
+                    width - 1 * px, y + 7 * px,
+                    width, y + 7 * px,
+                    width, y + 1 * px,
+                    width - 1 * px, y + 1 * px,
+                    width - 1 * px, y,
+                    width - 6 * px, y,
+                    width - 6 * px, y + 1 * px,
+                    width - 8 * px, y + 1 * px,
+                    width - 8 * px, y + 2 * px,
+                    0, y + 2 * px
+            });
+        } else {
+            model = new Polygon(new float[] {
+                    x + width, y + 6 * px,
+                    x + 8 * px, y + 6 * px,
+                    x + 8 * px, y + 7 * px,
+                    x + 6 * px, y + 7 * px,
+                    x + 6 * px, y + 8 * px,
+                    x + 1 * px, y + 8 * px,
+                    x + 1 * px, y + 7 * px,
+                    x, y + 7 * px,
+                    x, y + 1 * px,
+                    x + 1 * px, y + 1 * px,
+                    x + 1 * px, y,
+                    x + 6 * px, y,
+                    x + 6 * px, y + 1 * px,
+                    x + 8 * px, y + 1 * px,
+                    x + 8 * px, y + 2 * px,
+                    x + width, y + 2 * px
+            });
+        }
+        return Intersector.overlapConvexPolygons(parachutist.getModel(), model);
     }
 
     @Override
