1f083a828b3abdfe93a5a05dd3d5c2e68b32f4a5
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-29 01:42:30 -0500

    Added sensitivity setting

--- core/src/com/taugames/freefall/Settings.java
+++ core/src/com/taugames/freefall/Settings.java
@@ -20,7 +20,7 @@ public class Settings {
     public enum InputType {
         TOUCH, ROTATION
     }
-    
+
     public boolean rotationInputAvailable() {
         return Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer);
     }
@@ -38,5 +38,18 @@ public class Settings {
 
     public InputType getInputType() {
         return InputType.valueOf(preferences.getString("inputType", "TOUCH"));
+    }
+
+    // Sensitivity (sensitivity) settings
+    public void setSensitivity(float sensitivity) {
+        if (sensitivity <= 0 || sensitivity > 1) {
+            throw new IllegalArgumentException("The sensitivity is not allowed to be less than or equal to 0 or greater than 1.");
+        }
+        preferences.putFloat("sensitivity", sensitivity);
+        save();
+    }
+
+    public float getSensitivity() {
+        return preferences.getFloat("sensitivity", 0.5f);
     }
 }
