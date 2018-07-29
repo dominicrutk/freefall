dc1544e59ef7a5404e91a431c309db2250e32d95
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-29 00:53:15 -0500

    MainMenu code cleanup

--- core/src/com/taugames/freefall/MainMenu.java
+++ core/src/com/taugames/freefall/MainMenu.java
@@ -13,54 +13,54 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenu implements Screen {
+    private final Game game;
     private Stage stage;
 
-
     public MainMenu(final Game game) {
+        this.game = game;
 
-        float playLength = Gdx.graphics.getWidth() / 4f;
-        float iconLength = Gdx.graphics.getWidth() / 10f;
-        float iconDist = 20 * Gdx.graphics.getWidth() / 1440f;
+        float playButtonSize = Gdx.graphics.getWidth() / 4f;
+        float menuButtonSize = Gdx.graphics.getWidth() / 10f;
+        float menuButtonDistance = Gdx.graphics.getWidth() / 72f;
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        Texture playButton = game.getAssetManager().get("img/buttons/playButton.png", Texture.class);
-        ImageButton playButtonButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButton)));
-        playButtonButton.setSize(playLength, playLength);
-        playButtonButton.setPosition((Gdx.graphics.getWidth() - playLength) / 2f, (Gdx.graphics.getHeight() - playLength) / 2f);
-        playButtonButton.addListener(new ChangeListener() {
+        Texture playButtonTexture = game.getAssetManager().get("img/buttons/playButton.png", Texture.class);
+        ImageButton playButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(playButtonTexture)));
+        playButton.setSize(playButtonSize, playButtonSize);
+        playButton.setPosition((Gdx.graphics.getWidth() - playButtonSize) / 2f, (Gdx.graphics.getHeight() - playButtonSize) / 2f);
+        playButton.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 game.setScreen(new Infinite(game));
             }
         });
-        stage.addActor(playButtonButton);
+        stage.addActor(playButton);
 
-        Texture settingsButton = game.getAssetManager().get("img/buttons/settingsButton.png", Texture.class);
-        ImageButton settingsButtonButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(settingsButton)));
-        settingsButtonButton.setSize(iconLength, iconLength);
-        settingsButtonButton.setPosition(Gdx.graphics.getWidth() - iconDist - iconLength, Gdx.graphics.getHeight() - iconDist - iconLength);
-        settingsButtonButton.addListener(new ChangeListener() {
+        Texture settingsButtonTexture = game.getAssetManager().get("img/buttons/settingsButton.png", Texture.class);
+        ImageButton settingsButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(settingsButtonTexture)));
+        settingsButton.setSize(menuButtonSize, menuButtonSize);
+        settingsButton.setPosition(Gdx.graphics.getWidth() - menuButtonDistance - menuButtonSize, Gdx.graphics.getHeight() - menuButtonDistance - menuButtonSize);
+        settingsButton.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 Gdx.app.log("TODO", "Settings - Coming soon!");
             }
         });
-        stage.addActor(settingsButtonButton);
+        stage.addActor(settingsButton);
 
-        Texture shoppingCartButton = game.getAssetManager().get("img/buttons/shoppingCartButton.png", Texture.class);
-        ImageButton shoppingCartButtonButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(shoppingCartButton)));
-        shoppingCartButtonButton.setSize(iconLength, iconLength);
-        shoppingCartButtonButton.setPosition(iconDist, Gdx.graphics.getHeight() - iconDist - iconLength);
-        shoppingCartButtonButton.addListener(new ChangeListener() {
+        Texture shoppingCartButtonTexture = game.getAssetManager().get("img/buttons/shoppingCartButton.png", Texture.class);
+        ImageButton shoppingCartButton = new ImageButton(new TextureRegionDrawable(new TextureRegion(shoppingCartButtonTexture)));
+        shoppingCartButton.setSize(menuButtonSize, menuButtonSize);
+        shoppingCartButton.setPosition(menuButtonDistance, Gdx.graphics.getHeight() - menuButtonDistance - menuButtonSize);
+        shoppingCartButton.addListener(new ChangeListener() {
             @Override
             public void changed (ChangeEvent event, Actor actor) {
                 Gdx.app.log("TODO", "Shop - Coming soon!");
             }
         });
-        stage.addActor(shoppingCartButtonButton);
-
+        stage.addActor(shoppingCartButton);
     }
 
     @Override
@@ -70,13 +70,11 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-
         Gdx.gl.glClearColor(135f / 255, 206f / 255, 250f / 255, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act();
         stage.draw();
-
     }
 
     @Override
