ac8036e9f948031623645528e396e139a9ad4bf5
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-07-30 23:49:14 -0500

    Loaded font correctly

--- core/src/com/taugames/freefall/Game.java
+++ core/src/com/taugames/freefall/Game.java
@@ -1,8 +1,14 @@
 package com.taugames.freefall;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 
 public class Game extends com.badlogic.gdx.Game {
     private SpriteBatch spriteBatch;
@@ -14,6 +20,7 @@ public class Game extends com.badlogic.gdx.Game {
         spriteBatch = new SpriteBatch();
         assetManager = new AssetManager();
         settings = new Settings();
+        configureLoader();
         loadAssets();
         this.setScreen(new MainMenu(this));
     }
@@ -29,16 +36,31 @@ public class Game extends com.badlogic.gdx.Game {
         this.getScreen().dispose();
     }
 
+    private void configureLoader() {
+        FileHandleResolver fileHandleResolver = new InternalFileHandleResolver();
+        assetManager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(fileHandleResolver));
+        assetManager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(fileHandleResolver));
+    }
+
     private void loadAssets() {
-        assetManager.load("img/parachutist.png", Texture.class);
+        // Fonts
+        FreetypeFontLoader.FreeTypeFontLoaderParameter font = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
+        font.fontFileName = "font/PressStart2P.ttf";
+        font.fontParameters.size = 32;
+        assetManager.load("font/PressStart2P.ttf", BitmapFont.class, font);
 
-        assetManager.load("img/lasers/laserBody.png", Texture.class);
-        assetManager.load("img/lasers/laserEndLeft.png", Texture.class);
-        assetManager.load("img/lasers/laserEndRight.png", Texture.class);
-
+        // Icons
         assetManager.load("img/buttons/playButton.png", Texture.class);
         assetManager.load("img/buttons/settingsButton.png", Texture.class);
         assetManager.load("img/buttons/shoppingCartButton.png", Texture.class);
+
+        // Parachutist
+        assetManager.load("img/parachutist.png", Texture.class);
+
+        // Lasers
+        assetManager.load("img/lasers/laserBody.png", Texture.class);
+        assetManager.load("img/lasers/laserEndLeft.png", Texture.class);
+        assetManager.load("img/lasers/laserEndRight.png", Texture.class);
 
         assetManager.finishLoading();
     }
